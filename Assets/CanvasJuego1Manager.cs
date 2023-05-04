3ba84db3eb02be6ede90bd94c56032b3f703d3b2
dieegoalonso <48837581+dieegoalonso@users.noreply.github.com>
2023-05-05 01:05:32 +0200

    Funcionalidad botón empezar
    Al clicar en el botón empezar, se oculta la descripción del juego. Aún falta que el contador comience cuando se clique en el botón.

---
+++ Assets/CanvasJuego1Manager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasJuego1Manager : MonoBehaviour
+{
+    public void showCanvas(){
+        gameObject.SetActive(true);
+    }
+
+    public void hideCanvas(){
+        gameObject.SetActive(false);
+    }
+
+    public void toggleCanvas(){
+        if(gameObject.activeSelf == true){
+            gameObject.SetActive(false);
+        } else{
+            gameObject.SetActive(true);
+        }
+    }
+}
