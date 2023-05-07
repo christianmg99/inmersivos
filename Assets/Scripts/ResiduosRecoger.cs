d8d0fd847b28a71b3b586363b1b74c9f72e90728
Cesar Manuel Arrojo Lara <arrojocesar@gmail.com>
2023-05-07 20:27:34 +0200

    BETA Interaccion Directa recoger residuos los destruye

--- Assets/Scripts/ResiduosRecoger.cs
+++ Assets/Scripts/ResiduosRecoger.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ResiduosRecoger : MonoBehaviour
 {
+    public void Tocar(){
+        //Game object will turn off
+        Destroy(gameObject);
+        // Destroy(GetComponent<Rigidbody>(), 5);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
